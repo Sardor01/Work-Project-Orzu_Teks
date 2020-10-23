3ab0d67c3caee87fde32347a1676e1bf090c15c6
Sardor Astanov <developer_@a.sardor>
2020-10-23 09:12:16 +0500

    header changes made

--- src/shells/Header.vue
+++ src/shells/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="lg" class="main__navbar">
-    <div class="container-fluid py-2 px-1">
+    <div class="container-lg container-fluid-md py-2 px-1">
       <b-navbar-brand href="#" class="mr-auto ml-lg-5 ml-3">
         <img
           src="../assets/Logo/logo1.png"
@@ -10,7 +10,10 @@
         />
       </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle
+        target="nav-collapse"
+        class="navbar__collapseBtn"
+      ></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="mr-auto">
@@ -33,18 +36,18 @@
             <a href="#" class="nav__link">Контакты</a>
           </b-nav-item>
         </b-navbar-nav>
-
-        <div class="custom__select d-inline-block">
-          <select>
-            <option value="ru">RU</option>
-            <option value="en">EN</option>
-            <option value="uz">UZ</option>
-          </select>
-          <span class="custom__arrow">
-            <b-icon icon="chevron-down" variant="dark"></b-icon>
-          </span>
-        </div>
       </b-collapse>
+    </div>
+
+    <div class="custom__select d-inline-block">
+      <select>
+        <option value="ru">RU</option>
+        <option value="en">EN</option>
+        <option value="uz">UZ</option>
+      </select>
+      <span class="custom__arrow">
+        <b-icon icon="chevron-down" variant="dark"></b-icon>
+      </span>
     </div>
   </b-navbar>
 </template>
